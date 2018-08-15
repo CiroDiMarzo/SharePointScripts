a527f6b932dbc909ef4d4fbd01060ab908d58aec
Ciro di Marzo <ciro.dimarzo@gmail.com>
2018-08-15 15:06:20 +0000

    Rinominato Create-all in Deploy. Aggiunto script per Update-SPSolution

---
+++ Update-solution.ps1
@@ -0,0 +1,6 @@
+﻿
+$literalPath = "C:\Users\ciro\Documents\Solutions\061\AwesomeCalculator.wsp"
+
+Write-Host "Updating the solution $literalPath"
+
+Update-SPSolution -Identity AwesomeCalculator.wsp -LiteralPath $literalPath -GACDeployment
